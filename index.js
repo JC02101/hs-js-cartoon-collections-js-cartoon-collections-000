3e7479e06dd292affdc05659fa00c04d122fb335
Jess C <jessica.choi8902@gmail.com>
2019-04-26 19:49:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 function dwarfRollCall(dwarves) {
+   var array=[]
+  dwarves.forEach(function(dwarf,i) {
+  array.push(i+1 + ". " + dwarves[i] + " ")
+  })
+  return array.join("")
 }
+
 
 function summonCaptainPlanet(planeteerCalls){
+ return planeteerCalls.map(ele => ele.toUpperCase() + "!")
 }
+
 
 function longPlaneteerCalls(words) {
+	for(var i = 0; i < words.length; i++) {
+		if(words[i].length > 4) {
+			return true;
+		}
+	}
+	return false;
 }
 
+
 function findTheCheese (foods) {
+   for(var i = 0; i < foods.length; i++){
+  if (foods[i] == "cheddar" && "gouda" && "camembert"
+   ){
+    return foods[i]
+  }
 }
+return "no cheese!"
+}
+
